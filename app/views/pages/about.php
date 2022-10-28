797cc92676b669740a24660eb4093c85f2201728
Domitory123 <ddmitromaas@gmail.com>
2022-10-28 20:18:04 +0300

    add 3

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,3 +1,3 @@
 <h1> <?= "About Us Page" ?> </h1>
 <a class="btn btn-primary" href="<?= $_SERVER['PHP_SELF'] ?>?action=home" role="button">Головна</a>
-<p>Тут будуть відображатися відомості про нас2</p>
+<p>Тут будуть відображатися відомості про нас3</p>
